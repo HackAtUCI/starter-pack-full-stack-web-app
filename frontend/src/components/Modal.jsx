49b41781d3d9f8cc0d3889e0ddb6d85389530b1a
jellollama <chiucady107@gmail.com>
2025-11-08 18:04:42 -0800

    MODULOOOOO 1 and 2 works (using 2)

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Button, Dialog, DialogPanel, DialogTitle, Portal } from '@headlessui/react'
 import { useState } from 'react'
 
 
@@ -6,13 +6,14 @@ export const Modal = () => {
   let [isOpen, setIsOpen] = useState(false)
 
   return (
-    <>
+    <div>
       <Button onClick={() => setIsOpen(true)}>Open dialog</Button>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+      <Portal>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} static className="z-[9999]">
+        <div className="absolute inset-0 flex items-center justify-center p-4 bg-black/30">
           <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle className="font-bold">Deactivate account</DialogTitle>
-            <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
+            <DialogTitle className="font-bold">Topic</DialogTitle>
+            <p>info info info info.</p>
             <div className="flex gap-4">
               <Button onClick={() => setIsOpen(false)}>Cancel</Button>
               <Button onClick={() => setIsOpen(false)}>Deactivate</Button>
@@ -20,7 +21,8 @@ export const Modal = () => {
           </DialogPanel>
         </div>
       </Dialog>
-    </>
+      </Portal>
+    </div>
   )
 }
 
--- frontend/src/pages/results.jsx
+++ frontend/src/pages/results.jsx
@@ -1,10 +1,26 @@
 import Card from "@/components/Card";
 import Modal from "@/components/Modal";
 
-const Results = () => 
+import React, { useState } from "react";
+import ModalV2 from "@/components/ModalV2";
+
+const Results = () => {
+      const [isOpen, setIsOpen] = useState(false);
+    return (
+    <main>
+      <button onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+      {isOpen && <ModalV2 setIsOpen={setIsOpen} />}
+    </main>
+  );
+};
 <>
+<div>
     <Card /> 
-    <Modal />
+
+</div>
 </>
+
 
 export default Results;
